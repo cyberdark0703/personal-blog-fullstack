4f78f6759720cd17da66d723c38ac1d1f1ea8b96
Nguyễn Anh Đức <canhphong1103@gmail.com>
2026-09-06 14:02:00 +0700

    Create Entity User Class

---
+++ back_end/personal_blog/src/main/java/com/example/personal_blog/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.personal_blog.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
