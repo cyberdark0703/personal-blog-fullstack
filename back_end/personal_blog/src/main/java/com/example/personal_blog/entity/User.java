38a57c91b242f54618035558fddfa95d30df9c68
Nguyễn Anh Đức <canhphong1103@gmail.com>
2026-09-07 23:13:12 +0700

    Revert "test revert"
    This reverts commit dc926ac45a7f1a39ac03d9678c263f13fa82ec9f.

--- back_end/personal_blog/src/main/java/com/example/personal_blog/entity/User.java
+++ back_end/personal_blog/src/main/java/com/example/personal_blog/entity/User.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     private Long id;
-    private String username = "";
+    private String username;
     private String email;
     private String password;
     private String role;
